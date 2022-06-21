78f3a4c871ca0478b37d60fed6073a583038f1b7
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-21 15:32:34 +0100

    chore: created AppConatiner.tsx file to containalize app globally

---
+++ src/components/AppContainer.tsx
@@ -0,0 +1,21 @@
+import {NavigationContainer} from '@react-navigation/native';
+
+import {NativeBaseProvider} from 'native-base';
+
+import NativeBaseTheme from '../../theme';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const AppContainer = (props: Props) => {
+  return (
+    <NavigationContainer>
+      <NativeBaseProvider theme={NativeBaseTheme}>
+        {props.children}
+      </NativeBaseProvider>
+    </NavigationContainer>
+  );
+};
+
+export default AppContainer;
