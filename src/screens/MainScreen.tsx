56affeb1e157f6bb2d2d7e6ebaef395760ac1c10
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-21 16:45:04 +0100

    chore: Added StatusBar to follow colorScheme

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
 
-import { Box, Button, Center, Text, VStack } from 'native-base';
+import {
+  Box,
+  Button,
+  Center,
+  HStack,
+  StatusBar,
+  Switch,
+  Text,
+  useColorMode,
+  VStack,
+} from 'native-base';
 
 import ThemeToggler from '../components/ThemeToggler';
 
 const MainScreen = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Center
-      flex={1}
-      _dark={{ bg: 'blueGray.900' }}
-      _light={{ bg: 'blueGray.50' }}
-      px={4}>
-      <VStack space={5} alignItems={'center'}>
-        <Box>
-          <Text>Hello React Native</Text>
-          <Button>Submit</Button>
-        </Box>
-      </VStack>
-    </Center>
+    <>
+      <StatusBar
+        animated
+        barStyle={colorMode === 'light' ? 'dark-content' : 'light-content'}
+        backgroundColor={colorMode === 'light' ? '#fff' : '#223044'}
+      />
+      <Center
+        flex={1}
+        _dark={{ bg: 'blueGray.900' }}
+        _light={{ bg: 'blueGray.50' }}
+        px={4}>
+        <VStack space={5} alignItems={'center'}>
+          <Box>
+            <Text>Hello React Native</Text>
+            <Button>Submit</Button>
+          </Box>
+        </VStack>
+        <ThemeToggler />
+      </Center>
+    </>
   );
 };
 
