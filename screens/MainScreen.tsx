316366846ced7ff3c9316fdcc9b229164733d0a5
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-21 15:29:23 +0100

    chore: created first screen of the app

---
+++ screens/MainScreen.tsx
@@ -0,0 +1,23 @@
+import { Box, Button, Center, Text, VStack } from "native-base";
+import ThemeToggler from "../components/ThemeToggler";
+
+const MainScreen = () => {
+  return (
+    <Center
+      flex={1}
+      _dark={{ bg: "blueGray.900" }}
+      _light={{ bg: "blueGray.50" }}
+      px={4}
+    >
+      <VStack space={5} alignItems={"center"}>
+        <Box>
+          <Text>Hello React Native</Text>
+          <Button>Submit</Button>
+          <ThemeToggler />
+        </Box>
+      </VStack>
+    </Center>
+  );
+};
+
+export default MainScreen;
