0e6683e7622a3952ec3abf11d021d1baed244b08
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-21 16:05:38 +0100

    fix: import React for react-native to work well and enabled bracketSpacing in prettier config file

--- src/components/ThemeToggler.tsx
+++ src/components/ThemeToggler.tsx
@@ -1,11 +1,13 @@
-import { HStack, Switch, Text as NativeText, useColorMode } from "native-base";
+import React from 'react';
+
+import { HStack, Switch, Text as NativeText, useColorMode } from 'native-base';
 
 const ThemeToggler = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack space={2} alignItems="cehter">
       <NativeText>Dark</NativeText>
-      <Switch onToggle={toggleColorMode}></Switch>
+      <Switch onToggle={() => toggleColorMode} />
     </HStack>
   );
 };
