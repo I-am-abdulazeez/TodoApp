cc282ca6f417a72d6da0a57063786dccb0f0df46
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-21 17:11:52 +0100

    chore: App.tsx to default

--- App.tsx
+++ App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 
-import { StatusBar, useColorMode } from 'native-base';
-
 import AppContainer from './src/components/AppContainer';
 import MainScreen from './src/screens/MainScreen';
 
 const App = () => {
-  const { colorMode } = useColorMode();
   return (
     <AppContainer>
-      <StatusBar
-        animated
-        barStyle={colorMode === 'light' ? 'dark-content' : 'light-content'}
-        backgroundColor={colorMode === 'light' ? '#fff' : '#223044'}
-      />
       <MainScreen />
     </AppContainer>
   );
