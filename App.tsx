9eff94649331de65045d95f704fdc98999e076e7
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-21 17:03:53 +0100

    fix: implemented StatusBar on app globally

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import { StatusBar, useColorMode } from 'native-base';
+
 import AppContainer from './src/components/AppContainer';
 import MainScreen from './src/screens/MainScreen';
 
 const App = () => {
+  const { colorMode } = useColorMode();
   return (
     <AppContainer>
+      <StatusBar
+        animated
+        barStyle={colorMode === 'light' ? 'dark-content' : 'light-content'}
+        backgroundColor={colorMode === 'light' ? '#fff' : '#223044'}
+      />
       <MainScreen />
     </AppContainer>
   );
