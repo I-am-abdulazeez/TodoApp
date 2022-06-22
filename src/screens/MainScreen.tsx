83ce531c857da29a2d5a00903cb31a77f915ce1e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-06-22 17:26:57 +0100

    chore(ui): handling checkbox state from TaskItem component to MainScreen to for testing purpose

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import {
   Box,
   Button,
   Center,
+  Checkbox,
   StatusBar,
   Text,
   useColorMode,
@@ -11,16 +12,22 @@ import {
 } from 'native-base';
 
 import ThemeToggler from '../components/ThemeToggler';
+import TaskItem from '../components/TaskItem';
 
 const MainScreen = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleisChecked = useCallback(() => {
+    setIsChecked(prev => !prev);
+  }, []);
 
   return (
     <>
       <StatusBar
         animated
         barStyle={colorMode === 'light' ? 'dark-content' : 'light-content'}
-        backgroundColor={colorMode === 'light' ? '#fff' : '#223044'}
+        backgroundColor={colorMode === 'light' ? '#ffffff' : '#223044'}
       />
       <Center
         flex={1}
@@ -28,6 +35,9 @@ const MainScreen = () => {
         _light={{ bg: 'blueGray.50' }}
         px={4}>
         <VStack space={5} alignItems={'center'}>
+          <Box>
+            <TaskItem isDone={isChecked} onToggleCheckbox={handleisChecked} />
+          </Box>
           <Box>
             <Text>Hello React Native</Text>
             <Button>Submit</Button>
